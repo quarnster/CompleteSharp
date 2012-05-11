7d1affa7c759837938126cf782f18813b1383dd4
Fredrik Ehnbom <quarnster@gmail.com>
2012-05-11 14:50:28 +0200

    Latest SublimeCompletionCommon. Added license to the cs file

--- CompleteSharp.cs
+++ CompleteSharp.cs
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2012 Fredrik Ehnbom
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+*/
 using System;
 using System.Reflection;
 using System.Collections;
@@ -6,7 +28,7 @@ using System.Text.RegularExpressions;
 public class CompleteSharp
 {
     private static string sep = ";;--;;";
-    public static void Main(string[] arg)
+    public static void Main(string[] arg)
     {
         ArrayList assemblies = new ArrayList();
         if (arg.Length > 0)
